df7cc77a0e8f504471e7e36502099db3243d343f
Peter Kingston <46848056+peterjkingston@users.noreply.github.com>
2020-06-21 09:36:43 -0700

    Serializer tests written, all successful. Serializer tests DRYified.

--- Test_FolderSync/IO/Serializer.cs
+++ Test_FolderSync/IO/Serializer.cs
@@ -16,29 +16,101 @@ namespace Test_FolderSync.IO
 	[TestClass]
 	public class Serializer
 	{
+		SerializableAnimal _expectedAnimal = new SerializableAnimal("Dog", "Brown", 15, 35);
+		List<SerializableAnimal> _expectedAnimalList = new List<SerializableAnimal>(new SerializableAnimal[] { new SerializableAnimal("Dog", "Brown", 15, 35) });
+		ISerializer<SerializableAnimal> _animalSerializer = new Serializer<SerializableAnimal>();
+		ISerializer<List<SerializableAnimal>> _animalListSerializer = new Serializer<List<SerializableAnimal>>();
+		SerializationType _binary = SerializationType.Binary;
+		SerializationType _xml = SerializationType.XML;
+		SerializationType _json = SerializationType.JSON;
+
+		Action<string> wrapup = (path) => 
+		{ 
+			if (File.Exists(path))
+			{ 
+				File.Delete(path); 
+			} 
+		};
+
+		Func<SerializableAnimal, SerializableAnimal, bool> equal = (a, b) =>
+		{
+			return a.Name == b.Name &&
+					a.Color == b.Color &&
+					a.Height == b.Height &&
+					a.Weight == b.Weight;
+		};
+		
+		private T StandardDeserializeBinary<T>(string writePath) 
+		{
+			T actualAnimal;
+			using (FileStream fs = File.OpenRead(writePath))
+			{
+				actualAnimal = (T)new BinaryFormatter().Deserialize(fs);
+				fs.Close();
+			}
+			return actualAnimal;
+		}
+
+		private T StandardDeserializeXML<T>(string writePath)
+		{
+			T actualAnimal;
+			using (FileStream fs = File.OpenRead(writePath))
+			{
+				actualAnimal = (T)new XmlSerializer(typeof(T)).Deserialize(fs);
+				fs.Close();
+			}
+			return actualAnimal;
+		}
+
+		private T StandardDeserializeJson<T>(string writePath)
+		{
+			T actualAnimal;
+			using (TextReader textReader = new StreamReader(File.OpenRead(writePath)))
+			{
+				using (JsonReader jsonReader = new JsonTextReader(textReader))
+				{
+					actualAnimal = new JsonSerializer().Deserialize<T>(jsonReader);
+				}
+				textReader.Close();
+			}
+			return actualAnimal;
+		}
+
 		[TestMethod]
 		public void Serialize_WritesBinary()
 		{
 			//Arrange
-			SerializableAnimal expectedAnimal = new SerializableAnimal("Dog", "Brown", 15, 35);
-			FolderSync.IO.ISerializer<SerializableAnimal> serializer = new FolderSync.IO.Serializer<SerializableAnimal>();
 			string writePath = System.Reflection.MethodBase.GetCurrentMethod().Name;
-			SerializationType serializationType = SerializationType.Binary;
 			SerializableAnimal actualAnimal;
 			bool expected = true;
 
 			//Act
-			serializer.SerializeToFile(expectedAnimal, serializationType, writePath);
-			using(FileStream fs = File.OpenRead(writePath))
-			{
-				actualAnimal = (SerializableAnimal)new BinaryFormatter().Deserialize(fs);
-				fs.Close();
-			}
-			bool actual = expectedAnimal.Name == actualAnimal.Name &&
-						  expectedAnimal.Color == actualAnimal.Color &&
-						  expectedAnimal.Height == actualAnimal.Height &&
-						  expectedAnimal.Weight == actualAnimal.Weight;
-			File.Delete(writePath);
+			_animalSerializer.SerializeToFile(_expectedAnimal, _binary, writePath);
+			actualAnimal = StandardDeserializeBinary<SerializableAnimal>(writePath);
+
+			bool actual = equal(_expectedAnimal, actualAnimal);
+
+			wrapup(writePath);
+
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Serial_WritesBinaryArray()
+		{
+			//Arrange
+			string writePath = System.Reflection.MethodBase.GetCurrentMethod().Name;
+			SerializableAnimal actualAnimal;
+			bool expected = true;
+
+			//Act
+			_animalListSerializer.SerializeToFile(_expectedAnimalList, _binary, writePath);
+			actualAnimal = StandardDeserializeBinary<List<SerializableAnimal>>(writePath)[0];
+
+			bool actual = equal(_expectedAnimal, actualAnimal);
+
+			wrapup(writePath);
 
 			//Assert
 			Assert.AreEqual(expected, actual);
@@ -48,25 +120,37 @@ namespace Test_FolderSync.IO
 		public void Serialize_WritesXML()
 		{
 			//Arrange
-			SerializableAnimal expectedAnimal = new SerializableAnimal("Dog", "Brown", 15, 35);
-			FolderSync.IO.ISerializer<SerializableAnimal> serializer = new FolderSync.IO.Serializer<SerializableAnimal>();
 			string writePath = System.Reflection.MethodBase.GetCurrentMethod().Name;
-			SerializationType serializationType = SerializationType.XML;
 			SerializableAnimal actualAnimal;
 			bool expected = true;
 
 			//Act
-			serializer.SerializeToFile(expectedAnimal, serializationType, writePath);
-			using (FileStream fs = File.OpenRead(writePath))
-			{
-				actualAnimal = (SerializableAnimal)new XmlSerializer(typeof(SerializableAnimal)).Deserialize(fs);
-				fs.Close();
-			}
-			bool actual = expectedAnimal.Name == actualAnimal.Name &&
-						  expectedAnimal.Color == actualAnimal.Color &&
-						  expectedAnimal.Height == actualAnimal.Height &&
-						  expectedAnimal.Weight == actualAnimal.Weight;
-			File.Delete(writePath);
+			_animalSerializer.SerializeToFile(_expectedAnimal, _xml, writePath);
+			actualAnimal = StandardDeserializeXML<SerializableAnimal>(writePath);
+
+			bool actual = equal(_expectedAnimal, actualAnimal);
+
+			wrapup(writePath);
+
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Serial_WritesXMLArray()
+		{
+			//Arrange
+			string writePath = System.Reflection.MethodBase.GetCurrentMethod().Name;
+			SerializableAnimal actualAnimal;
+			bool expected = true;
+
+			//Act
+			_animalListSerializer.SerializeToFile(_expectedAnimalList, _xml, writePath);
+			actualAnimal = StandardDeserializeXML<List<SerializableAnimal>>(writePath)[0];
+
+			bool actual = equal(_expectedAnimal, actualAnimal);
+
+			wrapup(writePath);
 
 			//Assert
 			Assert.AreEqual(expected, actual);
@@ -76,32 +160,39 @@ namespace Test_FolderSync.IO
 		public void Serialize_WritesJSON()
 		{
 			//Arrange
-			SerializableAnimal expectedAnimal = new SerializableAnimal("Dog", "Brown", 15, 35);
-			FolderSync.IO.ISerializer<SerializableAnimal> serializer = new FolderSync.IO.Serializer<SerializableAnimal>();
 			string writePath = System.Reflection.MethodBase.GetCurrentMethod().Name;
-			SerializationType serializationType = SerializationType.JSON;
-			SerializableAnimal actualAnimal;
-			JsonSerializer js = new JsonSerializer();
 			bool expected = true;
 
 			//Act
-			serializer.SerializeToFile(expectedAnimal, serializationType, writePath);
-			using (TextReader textReader = new StreamReader(File.OpenRead(writePath)))
-			{
-				using (JsonReader jsonReader = new JsonTextReader(textReader))
-				{
-					actualAnimal = js.Deserialize<SerializableAnimal>(jsonReader);
-				}
-				textReader.Close();
-			}
-			bool actual = expectedAnimal.Name == actualAnimal.Name &&
-						  expectedAnimal.Color == actualAnimal.Color &&
-						  expectedAnimal.Height == actualAnimal.Height &&
-						  expectedAnimal.Weight == actualAnimal.Weight;
-			File.Delete(writePath);
+			_animalSerializer.SerializeToFile(_expectedAnimal, _json, writePath);
+
+			SerializableAnimal actualAnimal = StandardDeserializeJson<SerializableAnimal>(writePath);
+
+			bool actual = equal(_expectedAnimal, actualAnimal);
+			wrapup(writePath);
 
 			//Assert
 			Assert.AreEqual(expected, actual);
 		}
+
+		[TestMethod]
+		public void Serial_WritesJSONArray()
+		{
+			//Arrange
+			string writePath = System.Reflection.MethodBase.GetCurrentMethod().Name;
+			bool expected = true;
+
+			//Act
+			_animalListSerializer.SerializeToFile(_expectedAnimalList, _json, writePath);
+
+			SerializableAnimal actualAnimal = StandardDeserializeJson<List<SerializableAnimal>>(writePath)[0];
+
+			bool actual = equal(_expectedAnimal, actualAnimal);
+			wrapup(writePath);
+
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+
 	}
 }
